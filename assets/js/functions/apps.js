02d84927e9b8701ebc306abc35eddd647c21794f
proyect_icaro <josuevalenzuela87@gmail.com>
2021-08-02 16:52:52 -0400

    Actualización
    Modificacion de los archivos

--- assets/js/functions/apps.js
+++ assets/js/functions/apps.js
@@ -1,4 +1,4 @@
-/* Selectores */
+/* Selectores 
 const carrito = document.querySelector('#botonAgregar');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const listaProductos = document.querySelector('#lista-productos');
@@ -17,10 +17,50 @@ vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 	insertarCarritoHTML();
 })
 
-*/
+
 
 
 const detectarBotones = (data) => {
-    const botones = document.querySelectorAll('(${producto.id})')
+    const botones = document.querySelectorAll('.card')
     console.log(botones)
+}
+
+*/
+
+/* Selectores */
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const listaProductos = document.querySelector('#lista-productos');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+let articulosCarrito = [];
+
+const botonAgregar = document.getElementById('contenedorProductos');
+botonAgregar.addEventListener("click", agregarProducto);
+
+function detectar(){
+    alert("Estoy vivo!!!");
+}
+
+
+function agregarProducto(e) {
+    /* Evitamos la accion por defecto del boton */
+	e.preventDefault();
+	if (e.target.classList.contains("agregar-carrito")) {
+		/* Selecciono el card del producto sobre el que se hizo click */
+		const productoSeleccionado = e.target.parentElement.parentElement;
+     console.log("Producto agregado exitosamente");
+		//console.log(productoSeleccionado.querySelector('.precio span').textContent);
+		//obtenerDatosProducto(productoSeleccionado);
+	};
+}
+
+function eliminarProducto(e) {
+	if (e.target.classList.contains('borrar-producto')) {
+		const productoId = e.target.getAttribute('data-id');
+
+		articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
+
+		insertarCarritoHTML();
+		guardarStorage();
+	}
 }
--- assets/js/functions/funciones.js
+++ assets/js/functions/funciones.js
@@ -8,10 +8,10 @@ const cargarDatos = ()=>{
         <div class="col-md-3 offset-md-1 mb-2">
         <div class="card" styles="width: 18rem; id=${producto.id}">
             <div class="card-body">
-            <img src="${producto.img}" class="card-img--top"  style="width:200px"alt="${producto.titulo}"></a>
-                <h5 class="card-title">${producto.nombre}</h4>
-                <p class="card-text">Precio: ${producto.precio}</p>
-                <a href="#" class="btn btn-primary" id="botonAgregar" onclick="agregarAlCarrito(${producto.id})">Agregar al Carrito</a>
+            <img src="${producto.img}" class="card-img--top item" style="width:200px"alt="${producto.titulo}"></a>
+                <h5 class="card-title item-title">${producto.nombre}</h4>
+                <p class="card-text item-price">Precio: ${producto.precio}</p>
+                <a  class="btn btn-primary addToCart" onclick="agregarAlCarrito(${producto.id}")>Agregar al Carrito</a>
             </div>
         </div>
     </div>`
@@ -19,3 +19,5 @@ const cargarDatos = ()=>{
         $("#contenedorProductos").html(vistaProductos);
     });
 };
+//Lo voy a dejar reservado acá mientras tanto
+/*onclick="agregarAlCarrito(${producto.id}")*/
